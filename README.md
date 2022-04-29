3c8513368fb92928593de65ecb15af2d9e0c0499
Asad <asad.khan.163631@gmail.com>
2022-04-29 05:59:59 +0100

    improved API testing and fleshed out readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # AirQualitySensors
+
+## Deployment Of Testing Environment
+
+1. Ensure [Docker](https://www.docker.com/) is installed
+2. Clone the repo
+3. Navigate to the project root in a terminal window
+4. Run the command ```docker compose -f docker-compose-test-env.yml up -d```
+5. Wait until Docker builds and deploys containers.
+
+## Usage
+
+- Either ```plume```, ```zephyr``` or ```sensor_community``` buckets can be queried through the API end point available
+  on ```http://0.0.0.0:8000```
+
+1. POST ```http://0.0.0.0:8000/api/tasks/create/{sensor}```, where ```{sensor}``` is one of the above-mentioned buckets,
+   include a ```start``` and ```end``` timestamp in the request payload
+2. A task id will be returned the following json format; ```{"task_id": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"}```
+3. GET ```http://0.0.0.0:8000/api/tasks/{task_id}``` where the ```task_id``` is the task id returned from the previous
+   POST
+4. A status and result will be returned in the following json
+   format; ```{"task_id": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", "task_status": {status}, task_result: {task_result}}```
+5. The ```{status}``` will either be ```SUCESS```, ```PENDING``` or ```FAILED```
+6. If ```PENDING``` the GET can be re-requested to check the status until the back end task reports a success or failure
+7. ```{task_result}```will be populated when the task succeeds in the following json
+   format; ```[{"sensor_id": {sensor_id}, "header": {header}, "rows": [rows]}]```
+
+## Administration
+
+- Flower monitoring server available on ```http://0.0.0.0:8888```
+- Influx DB available on ```http://0.0.0.0:8086```
--- test/api_tests.py
+++ test/api_tests.py
@@ -1,5 +1,8 @@
 import datetime as dt
+import sys
+import time
 
+import pandas
 import requests
 
 local_test_endpoint = "http://0.0.0.0:8000"
@@ -8,15 +11,45 @@ local_test_endpoint = "http://0.0.0.0:8000"
 def create_task(sensor):
     return requests.post(f"{local_test_endpoint}/api/tasks/create/{sensor}",
                          data={"start": dt.datetime(2021, 1, 1).timestamp(),
-                               "end": dt.datetime(2021, 6, 1).timestamp()})
+                               "end": dt.datetime(2022, 1, 1).timestamp()})
 
 
 def get_task(task_id):
-    res = requests.get(f"{local_test_endpoint}/api/tasks/{task_id}")
-    print(res.text)
+    return requests.get(f"{local_test_endpoint}/api/tasks/{task_id}").json()
+
+
+def to_df(json_):
+    result = json_["task_result"]
+    for sensor in result:
+        yield sensor["sensor_id"], pandas.DataFrame(sensor["rows"], columns=sensor["header"])
+
+
+def check_status(json_):
+    return json_["task_status"]
+
+
+def test(bucket):
+    print("Testing Bucket:", bucket)
+    task_id = create_task(bucket).json()["task_id"]
+    print("Task ID:", task_id)
+    print("Checking task for next 20 second")
+    for _ in range(20):
+        time.sleep(1)
+        task = get_task(task_id)
+        status = check_status(task)
+        print("Task Status:", status)
+        if status == "SUCCESS":
+            for id_, df in to_df(task):
+                print("Sensor ID:", id_)
+                print(df)
+            return
+    print("Timeout", file=sys.stderr)
 
 
 if __name__ == '__main__':
-    # res = create_task("plume")
-    # print(res.text)
-    get_task("0aa9e596-15bc-4c21-86dc-8d006110563b")
+    """
+    Retrieve all data available for each bucket between 2021 - 2022
+    """
+    test("plume")
+    test("zephyr")
+    test("sensor_community")
